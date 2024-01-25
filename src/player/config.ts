4ed07693e4a4a766a1eb11c3e6e73379acbe9f47
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-01-25 11:20:38 -0500

    document config

--- src/player/config.ts
+++ src/player/config.ts
@@ -2,6 +2,8 @@
 // and the effective framerate drops
 export const USE_BITMAP_RENDERER_CANVAS = true;
 
+// should we ask for hw accel when initializing video decoder?
 export const REQUEST_HARDWARE_ACCELERATION = true;
 
+// should we pass optimizeForLatency flag when initializing video decoder?
 export const OPTIMIZE_FOR_LATENCY_FLAG = true;
