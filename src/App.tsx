5daf7a2f37625d1a3717d89ef4c0c43a68a6a847
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-01-25 18:05:46 -0500

    reorder videos

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import { Metrics } from './Metrics';
 import { isWebCodecsSupported } from './isWebcodecsSupported';
 
 const mockAdResponse: AdPod[] = [
+  // grubhub 480p 24fps AVC
+  {
+    video:
+      'https://ark.tubi.video/24877d7d-a7cc-4045-82d0-e6b0069b831e/854x480_1200k.mp4',
+  },
   // febreeze 480p 24fps AVC
   {
     video:
@@ -14,11 +19,6 @@ const mockAdResponse: AdPod[] = [
   {
     video:
       'https://ark.tubi-staging.video/v2/cb526b6f284b76e795ab90e876278ad6/f46fa51f-0cd4-4b0b-bed8-4eb35bd35ba9/1920x1080_3400k.mp4',
-  },
-  // grubhub 480p 24fps AVC
-  {
-    video:
-      'https://ark.tubi.video/24877d7d-a7cc-4045-82d0-e6b0069b831e/854x480_1200k.mp4',
   },
 ];
 
