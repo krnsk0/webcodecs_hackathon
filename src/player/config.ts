2b8c863dcc06f4b032cc0a1b239e8df22a161a61
Jonathan Kurinsky <jkurinsky@tubi.tv>
2024-01-25 18:06:21 -0500

    update default config

--- src/player/config.ts
+++ src/player/config.ts
@@ -1,6 +1,6 @@
 // without this the commit phase in profiler is very long
 // and the effective framerate drops
-export const USE_BITMAP_RENDERER_CANVAS = false;
+export const USE_BITMAP_RENDERER_CANVAS = true;
 
 // should we ask for hw accel when initializing video decoder?
 export const REQUEST_HARDWARE_ACCELERATION = true;
@@ -9,16 +9,16 @@ export const REQUEST_HARDWARE_ACCELERATION = true;
 export const OPTIMIZE_FOR_LATENCY_FLAG = true;
 
 // how many ms should we attempt to prebuffer?
-export const PREBUFFER_TARGET = 500;
+export const PREBUFFER_TARGET = 1000;
 
 // how far ahead of the playhead should we buffer?
-export const BUFFER_TARGET = 1000;
+export const BUFFER_TARGET = 500;
 
 // how many workers to use for frame conversion?
 export const FRAME_CONVERSION_WORKERS = 1;
 
 // how many frames behind the playhead should we start purge frames in the buffer?
-export const FRAME_PURGE_THRESHOLD = 10;
+export const FRAME_PURGE_THRESHOLD = 4;
 
 // how fast to update the visualizations / metrics
 export const UI_UPDATE_INTERVAL = 100;
